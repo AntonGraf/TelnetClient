1751c6fe0edd9dbe04e4c2eca59160bafa5d1852
AntonGraf <ant0ngraf@yandex.ru>
2023-04-05 09:06:21 +0600

    Initial Commit

---
+++ src/main/java/ru/tetra/telnetclient/TelnetClientApplication.java
@@ -0,0 +1,13 @@
+package ru.tetra.telnetclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelnetClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelnetClientApplication.class, args);
+    }
+
+}
